ca70815d90ebca7110d629a75757fe869221b0cd
Daniel Ågstrand <d.e.agstrand@gmail.com>
2017-11-14 14:33:11 +0100

    Cont. on bin multi

--- binary_mult.py
+++ binary_mult.py
@@ -8,10 +8,50 @@ Student Names: Daniel Ågstrand, Linnea Andersson
 '''
 import unittest
 
-def shift(seq, n):
-    return seq[n:]+seq[:n]
+def shift_left(l, n):
+    return l[n:]+l[:n]
 
 
+def shift_right(l, n):
+    if len(l) == 1:
+        return [0]
+    for i in range(len(l)-1):
+        l[i+1] = l[i]
+    for j in range(n):
+        l[j] = 0
+    return l
+
+
+def half_adder(A,B):
+    C = [0] * len(A)
+    Carry = 0
+    A = list(reversed(A))
+    B = list(reversed(B))
+    if len(A) == len(B):
+        for i in range(len(A)):
+            if A[i] == B[i] and Carry == 0:
+                if A[i] == 0:
+                    C[i] = 0
+                else:
+                    C[i] = 0
+                    Carry = 1
+            elif A[i] == B[i]:
+                if A[i] == 1:
+                    C[i] = 1
+                    Carry = 1
+                else:
+                    C[i] = 1
+                    Carry = 0
+            else:
+                if Carry == 1:
+                    C[i] = 0
+                else:
+                    C[i] = 1
+                    Carry = 0
+        return list(reversed(C))
+    else:
+        return -1
+ 
 
 def binary_mult(A,B):
     """
@@ -21,38 +61,41 @@ def binary_mult(A,B):
     Var:    
     Example:    binary_mult([0,1,1],[1,0,0]) = [0,0,1,1,0,0]
     """
-    global n
+    global i
     n = len(A)
-    i = 0
     if len(A) == len(B):
-        print 'n = ', format(n)
         if n == 1:
-            i = i + 1
-            print 'i = ', format(i)
-            return [A[0] * B[0]] 
+            return [0,(A[0] * B[0])] 
         else:
             a1 = A[0:n/2]
             b1 = A[0:n/2]
             a2 = A[n/2:n]
             b2 = A[n/2:n]
 
+            #print 'a1 = ', format(a1)
+            #print 'a2 = ', format(a2)
+            #print 'b1 = ', format(b1)
+            #print 'b2 = ', format(b2)
 
             x1 = binary_mult(a2,b2)
             x2 = binary_mult(a2,b1)
             x3 = binary_mult(a1,b2)
             x4 = binary_mult(a1,b1)
 
-            #print x1
-            #print x2
-            #print x4
+            x2 = half_adder(x2,x3)
+            x1 = shift_left(x1, n)
+            x2 = shift_right(x2, n/2)
+         
+        temp = half_adder(x1, x2)
+        temp = half_adder(temp, x4)
 
-            x2 = x2 + x3
-            x1 = shift(x1, n)
-            x2 = shift(x2, n/2)
-
-        return [x1[i] + x2[i] + x4[i] for i in range(min(len(x1), len(x2), len(x4)))]
+        #print temp
+        for i in range(n):
+            temp = [0] + temp
+        return temp
     else:
         return -1
+    
     
 class BinaryMultTest(unittest.TestCase):
     """Test Suite for binary multiplication problem
@@ -69,8 +112,8 @@ class BinaryMultTest(unittest.TestCase):
         This is a simple sanity check for your function;
         passing is not a guarantee of correctness.
         """
-        A = [0,1,0,1]
-        B = [0,1,1,0]
+        A = [0,1,1,0]
+        B = [0,0,1,0]
         answer = binary_mult(A, B)
         self.assertEqual(answer, [0,0,0,0,1,1,0,0])
 
