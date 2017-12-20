4a209ed44808f551f6327b695606a2edb3dc6c9b
Daniel Ågstrand <d.e.agstrand@gmail.com>
2017-12-20 12:35:45 +0100

    All done

--- Assignment 3/party_seating.py
+++ Assignment 3/party_seating.py
@@ -36,11 +36,6 @@ def party(known):
         for j in range(len(known[i])):
             #Variant: len(known[i]) - j
             G.add_edge(i,known[i][j])
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G,pos)
-    nx.draw_networkx_edges(G,pos)
-    nx.draw_networkx_labels(G,pos)
-    plt.show()
     nx.set_node_attributes(G,'color','white')
     nodes = nx.get_node_attributes(G,'color')
     bol = BFS_color(G)
@@ -53,9 +48,7 @@ def party(known):
                 table1.append(i)
             else:
                 table2.append(i)
-    print table1
-    print table2
-    print bol
+
     return bol, table1, table2
 
 def BFS_color(G):
@@ -70,14 +63,11 @@ def BFS_color(G):
         if nodes[s] is 'white':
             queue.append(s)
             nodes[s] = 'red'
-        print nodes
         while queue != []:
             #Variant: queue 
             u = queue.pop()
             for n in G.neighbors(u):
                 #Variant: G.neighbors(u) - n 
-                print u
-                print n
                 if nodes[n] == 'white' and nodes[u] == 'red':
                     nodes[n] = 'blue'
                     queue.append(n)
@@ -89,50 +79,3 @@ def BFS_color(G):
                 elif nodes[n] == 'red' and nodes[u] == 'red':
                     return False
     return True
-
-
-class PartySeatingTest(unittest.TestCase):
-    """Test suite for party seating problem
-    """
-
-    def test_sanity(self):
-        """Sanity test
-
-        A minimal test case.
-        """
-        K = [[11, 12], [], [], [], [], [], [], [], [], [], [], [0], [0]]
-        (found, A, B) = party(K)
-        self.assertEqual(
-            len(A) + len(B),
-            len(K),
-            "wrong number of guests: {!s} guests, tables hold {!s} and {!s}".format(
-                len(K),
-                len(A),
-                len(B)
-                )
-            )
-        for g in range(len(K)):
-            self.assertTrue(
-                g in A or g in B,
-                "Guest {!s} not seated anywhere".format(g))
-        for a1 in A:
-            for a2 in A:
-                self.assertFalse(
-                    a2 in K[a1],
-                    "Guests {!s} and {!s} seated together, and know each other".format(
-                        a1,
-                        a2
-                        )
-                    )
-        for b1 in B:
-            for b2 in B:
-                self.assertFalse(
-                    b2 in K[b1],
-                    "Guests {!s} and {!s} seated together, and know each other".format(
-                        b1,
-                        b2
-                        )
-                    )
-
-if __name__ == '__main__':
-    unittest.main()
